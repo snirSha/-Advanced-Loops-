0503a67e9848091abf801877b8c14c5cfb3e2bf8
snir <46107190+snirSha@users.noreply.github.com>
2019-05-23 11:54:39 +0300

    Update powerset.hpp

--- powerset.hpp
+++ powerset.hpp
@@ -3,33 +3,85 @@
 #include <iostream>
 #include <set>
 #include <cmath>
+#include <vector>
+
+using namespace std;
 
 namespace itertools{
 
-	template<class T> class Powerset{
+	template<class T> class powerset{
 		private:
-			T iter;
-		template <typename T1> class iterator{
-			public:
-				T1 first;
-				iterator(T1 x):first(x){}
-		
-		};
+			T tank;
+
 		public:
-			Powerset(T start): iter(start){}
-			
-			auto begin() { 
-    				return iter.begin(); 
-    			} 
-    			auto end()  { 
-    				return iter.end();
-			} 	
+			powerset(const T ob): tank(ob){}
+
+			template<typename E> class iterator{
+			private:
+						E start;
+						E last;
+						size_t setLength;
+						uint index;
+
+			public:
+						iterator(E s,E l):start(s),last(l),setLength(0),index(0){
+							size_t counter=0;
+							E temp=start;
+							while(temp!=last){
+								++counter;
+								++temp;
+							}
+							setLength=pow(2,counter);//there are 2^tank.length sets
+						}
+
+						//operator *
+						auto operator* () const{
+							std::vector<typename std::remove_const<typename std::remove_reference<decltype(*start)>::type>::type> Set;
+							E temp=start;
+							uint i=index;
+							while(temp!=last&&i!=0){
+								if(i%2==1){//1 insert element, 0 end of Set
+									Set.emplace_back(*temp);
+								}
+								i>>=1;//shift left
+								++temp;
+							}
+							return Set;
+						}
+
+						//operator ++
+						auto operator++ (){
+							++index;
+							return *this;
+						}
+
+						//operator !=
+						auto operator!=(iterator<E> const &next) const{
+							return (setLength-index!=next.setLength-next.index-1);
+						}
+			};
+
+			auto begin() const{
+    				return iterator<decltype(tank.begin())>(tank.begin(),tank.end());
+    	}
+    	auto end() const{
+    				return iterator<decltype(tank.end())>(tank.end(),tank.end());
+			}
 	};
 
-	template <typename T> Powerset<T> powerset(T A){
-		return Powerset<T>(A);
-	}
-	template <typename T> std::ostream &operator<<(std::ostream &os,std::set<T> &Set){
+
+	template <typename T> std::ostream &operator<<(std::ostream &os,const std::vector<T> &printSet){
+		os<<"{";
+		auto it=printSet.begin();
+		if(it!=printSet.end()){
+			os<<*it;
+			++it;
+		}
+		while(it!=printSet.end()){
+			os<<','<<*it;
+			++it;
+		}
+		os<<"}";
 		return os;
 	}
 }
