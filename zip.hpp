1875f04c94aa0f96b7e4d98fbb417e1301104fe8
snir <46107190+snirSha@users.noreply.github.com>
2019-05-24 11:26:43 +0300

    Update zip.hpp

--- zip.hpp
+++ zip.hpp
@@ -3,7 +3,7 @@
 
 namespace itertools{
 
-template <typename R, typename S> class Zip{
+template <typename R, typename S> class zip{
 
 	private: 
 		R iter1;
@@ -21,32 +21,29 @@ template <typename R, typename S> class Zip{
 				return std::pair<decltype(*first),decltype(*second)> (*first,*second);
 			}
 			//operator ++
-			Zip::iterator<T1,T2> &operator++(){
+			zip::iterator<T1,T2> &operator++(){
 				++first;
 				++second;
 				return *this;			
 			}
 			//operator !=
-			bool operator!=(Zip::iterator<T1,T2> const &next){
+			bool operator!=(zip::iterator<T1,T2> const &next){
 				return ((first!=next.first)&&(second!=next.second));
 			}
 	};	
 
 	public: 
-		Zip(R start ,S end): iter1(start),iter2(end){}
+		zip(R start ,S end): iter1(start),iter2(end){}
 
 		auto begin() const{
-			return Zip::iterator<decltype(iter1.begin()),decltype(iter2.begin())>(iter1.begin(),iter2.begin());
+			return zip::iterator<decltype(iter1.begin()),decltype(iter2.begin())>(iter1.begin(),iter2.begin());
 		}
 		auto end() const{
-			return Zip::iterator<decltype(iter1.end()),decltype(iter2.end())>(iter1.end(),iter2.end());
+			return zip::iterator<decltype(iter1.end()),decltype(iter2.end())>(iter1.end(),iter2.end());
 		}
 		
 };
-//zip function
-template <typename T1,typename T2> Zip<T1,T2> zip(T1 A,T2 B){
-	return Zip<T1,T2>(A,B);
-}
+
 //print pair
 template <typename T1,typename T2> std::ostream &operator<<(std::ostream &os,const std::pair<T1,T2> &str){
 os<<str.first<<','<<str.second;
